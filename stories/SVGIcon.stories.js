060a464b29a9fb3bc5a6150f817d3f2940aa5df8
SVISHNEV <shlomo-stanisla.vishnevetskiy@amdcocs.com>
2017-05-21 11:51:27 +0300

    icon select option selector

--- stories/SVGIcon.stories.js
+++ stories/SVGIcon.stories.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import Examples from './utils/Examples.js';
+import {select} from '@kadira/storybook-addon-knobs';
+
 
 import SVGIcon from '../src/react/SVGIcon';
 // import HTMLButtonPrimary from '../components/button/button-primary.html';
 // import HTMLButtonWhite from '../components/button/button-white.html';
 
-let examples = {
-        Primary: {
-            jsx: <SVGIcon label='test1' name='vendor'/>
-        }        
-    };
 
-const Icons = () => (
-    <Examples examples={examples} />
-);
+
+
+const icons = [
+    'vendor',
+    'vsp',
+    'vlm'
+];
+
+function selectType() {
+    return select('Item type' , icons, icons[0]);
+}
+
+
+function buildExamples(iconName) {
+    return {
+        Primary: {
+            jsx: <SVGIcon label='test' name={iconName} />
+        }  
+    }
+}
+
+class Icons extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            iconName: 'vendor'
+        }
+    }
+    render() {
+        return (  
+            <div>
+                <select onChange={e => this.setState({iconName: e.target.value})}>{icons.map(option => <SelectOption option={option}/>)}</select>
+                <Examples examples={buildExamples(this.state.iconName)} />    
+           </div> 
+        )
+    };
+}
+
+const SelectOption = ({option}) => {
+    return (
+        <option value={option}>{option}</option>
+    );
+}
 
 export default Icons;
