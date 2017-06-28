0ff99036fe53ee25d39bc91ab9b5205f01d4f404
Omri Kochavi <nomrik@gmail.com>
2017-06-28 18:32:35 +0300

    add other props support to button and fix other support in svg

--- src/react/Button.js
+++ src/react/Button.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SVGIcon from './SVGIcon.js';
 
-const Button = ({btnType = 'default', color = 'primary', onClick, disabled, iconName, children}) => (
-  <button onClick={onClick} className={`sdc-button sdc-button-${btnType} sdc-button__${color} ${iconName ? 'sdc-button__with-icon' : ''}`} disabled={disabled}>
-    {iconName && <SVGIcon name={iconName} />}
-    {children}
-  </button>
-);
+const Button = (props) => {
+	let {btnType = 'default', color = 'primary', onClick, disabled, iconName, children, ...other} = props;
+	return (
+    <button onClick={onClick} className={`sdc-button sdc-button-${btnType} sdc-button__${color} ${iconName ? 'sdc-button__with-icon' : ''}`} disabled={disabled} {...other}>
+      {iconName && <SVGIcon name={iconName} />}
+      {children}
+    </button>
+	);
+};
 
 export default Button;
