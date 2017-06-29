6d59c4463e69505cf047ba6b32fe7b73b082e872
zivl <levi.ziv@gmail.com>
2017-06-29 14:29:37 +0300

    font adoption and fixes

--- stories/Typography.stories.js
+++ stories/Typography.stories.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 
 const typos = [
-	{className: 'heading-1', size: 36, text: 'Large Titles'},
+	{className: 'heading-1', size: 28, text: 'Large Titles'},
 	{className: 'heading-2', size: 24, text: 'Medium Titles'},
-	{className: 'heading-4-medium', size: 18, text: 'Small Titles'},
-	{className: 'heading-5-medium', size: 16, text: 'Mini Titles'},
+	{className: 'heading-4-semibold', size: 18, text: 'Small Titles'},
+	{className: 'heading-5-semibold', size: 16, text: 'Mini Titles'},
 	{className: 'body-1', size: 14, text: 'Body (Running) Text'},
 	{className: 'body-2', size: 13, text: 'Small Text'},
 	{className: 'body-3', size: 12, text: 'Even Smaller Text'},
+	{className: 'body-4', size: 10, text: 'Even Much Smaller Text'}
 ];
 
 const fontWeights = ['OpenSans', 'OpenSans-Light', 'OpenSans-Italic', 'OpenSans-Semibold' ,'OpenSans-Bold'];
