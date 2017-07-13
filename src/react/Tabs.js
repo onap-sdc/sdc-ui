f14b1124f3955ef2c214bb67917384379f801e2f
Omri Kochavi <nomrik@gmail.com>
2017-07-13 15:40:24 +0300

    add disabled support for tabs + accept custom className for all components

--- src/react/Tabs.js
+++ src/react/Tabs.js
@@ -3,9 +3,9 @@ import TabPane from './TabPane.js';
 
 class Tabs extends React.Component {
 	render() {
-		const {type, children = [], activeTab, onTabClick} = this.props;
+		const {type, children = [], activeTab, onTabClick, className} = this.props;
 		return (
-			<div className={type === 'header' ? 'sdc-tabs sdc-tabs-header' : 'sdc-tabs sdc-tabs-menu'} >
+			<div className={type === 'header' ? `sdc-tabs sdc-tabs-header ${className || ''}` : `sdc-tabs sdc-tabs-menu ${className || ''}`} >
 				<ul className='sdc-tabs-list' role='tablist'>
 					{children.map(child => React.cloneElement(child,
 						{
