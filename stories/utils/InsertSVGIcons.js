44244f098490ccac39ae3d75162722334a43a20e
Ariel Kenan <arielke@amdocs.com>
2017-07-30 11:39:04 +0300

    created util to extract svg code from rendered jsx and insert it into html code

---
+++ stories/utils/InsertSVGIcons.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {renderToStaticMarkup} from 'react-dom/server';
+import BeautifyHTML from './BeautifyHTML.js';
+
+const InsertSVGIcons = ({html, jsx, indentChar = '    '}) => {
+	let svgCode = renderToStaticMarkup(jsx).match(/(<svg\b[^<>]*>)[\s\S]*?(<\/svg>)/g);
+	let newHTML = html.replace(/\s*<!-- insert SVG -->/g, str => {
+		let html = '\n' + svgCode.shift();
+		let indentRegExp = new RegExp('[' + indentChar + ']*');
+		let startingIndentCount = str.slice(2).match(indentRegExp)[0].length / indentChar.length;
+		return BeautifyHTML({html, startingIndentCount, indentChar});
+	});
+	return newHTML;
+};
+
+export default InsertSVGIcons;
