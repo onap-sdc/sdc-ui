69a01975557548967f74708ec322d895cad5a58e
navke <ewk_80@server.fake>
2017-08-10 13:29:14 +0300

    added icons related to collaboration and did some es-linting all over.

--- stories/utils/InsertSVGIcons.js
+++ stories/utils/InsertSVGIcons.js
@@ -1,16 +1,15 @@
-import React from 'react';
 import {renderToStaticMarkup} from 'react-dom/server';
-import BeautifyHTML from './BeautifyHTML.js';
+import beautifyHTML from './BeautifyHTML.js';
 
-const InsertSVGIcons = ({html, jsx, indentChar = '    '}) => {
+const insertSVGIcons = ({html, jsx, indentChar = '    '}) => {
 	let svgCode = renderToStaticMarkup(jsx).match(/(<svg\b[^<>]*>)[\s\S]*?(<\/svg>)/g);
 	let newHTML = html.replace(/\s*<!-- insert SVG -->/g, str => {
 		let html = '\n' + svgCode.shift();
 		let indentRegExp = new RegExp(`[${indentChar}]*`);
 		let startingIndentCount = str.slice(2).match(indentRegExp)[0].length / indentChar.length;
-		return BeautifyHTML({html, startingIndentCount, indentChar});
+		return beautifyHTML({html, startingIndentCount, indentChar});
 	});
 	return newHTML;
 };
 
-export default InsertSVGIcons;
+export default insertSVGIcons;
