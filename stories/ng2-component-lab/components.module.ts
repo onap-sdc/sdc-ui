2622f360efc240319a494b6339825b35c896c0e6
Orit Barda <ob0695@intl.att.com>
2017-10-10 10:15:38 +0300

    [US135] renaming angular2 folder to angular

--- stories/ng2-component-lab/components.module.ts
+++ stories/ng2-component-lab/components.module.ts
@@ -2,10 +2,9 @@ import {NgModule, Injector} from "@angular/core";
 import {KeysPipe} from "./utils/pipes/keys.pipe";
 import {CommonModule} from "@angular/common";
 import {ColorsTable} from "./components/colors-table.component";
-import {FormElementsModule} from "../../src/angular2/form-elements/form-elements.module";
-import {ButtonsModule} from "../../src/angular2/buttons/buttons.module";
+import {FormElementsModule} from "../../src/angular/form-elements/form-elements.module";
+import {ButtonsModule} from "../../src/angular/buttons/buttons.module";
 
-export let AppInjector: Injector;
 
 @NgModule({
     declarations: [
