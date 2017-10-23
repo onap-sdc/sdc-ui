86503415adc0ba64d5eb33ad1e9d56c3cf34b624
Eli Chen <ec063q@intl.att.com>
2017-10-23 18:04:57 +0300

    Popup menu list component.

---
+++ src/angular/popup-menu/popup-menu.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from "@angular/core";
+import {PopupMenuListComponent} from "./popup-menu-list.component";
+import {PopupMenuItemComponent} from "./popup-menu-item.component";
+import {CommonModule} from "@angular/common";
+
+
+@NgModule({
+    declarations: [
+        PopupMenuListComponent,
+        PopupMenuItemComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        PopupMenuListComponent,
+        PopupMenuItemComponent
+    ],
+})
+export class PopupMenuModule {
+}
