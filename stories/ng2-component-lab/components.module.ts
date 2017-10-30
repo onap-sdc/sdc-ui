e8b181b535beef114d68956ee656e17a6c93e806
Eli Chen <ec063q@intl.att.com>
2017-10-30 18:08:06 +0200

    Added showcase for font icons.

--- stories/ng2-component-lab/components.module.ts
+++ stories/ng2-component-lab/components.module.ts
@@ -1,23 +1,28 @@
 import {NgModule, Injector} from "@angular/core";
-import {KeysPipe} from "./utils/pipes/keys.pipe";
 import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {KeysPipe} from "./utils/pipes/keys.pipe";
 import {ColorsTable} from "./components/colors-table.component";
+import {FontIconsShowcaseComponent} from "./components/font-icons-showcase.component";
 import {FormElementsModule} from "../../src/angular/form-elements/form-elements.module";
 import {ButtonsModule} from "../../src/angular/buttons/buttons.module";
 
 @NgModule({
     declarations: [
         ColorsTable,
-        KeysPipe
+        KeysPipe,
+        FontIconsShowcaseComponent
     ],
     imports: [
         CommonModule,
+        FormsModule,
         FormElementsModule,
         ButtonsModule
     ],
     entryComponents: [],
     exports: [
         ColorsTable,
+        FontIconsShowcaseComponent,
         FormElementsModule,
         ButtonsModule
     ],
