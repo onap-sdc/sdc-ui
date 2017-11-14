a8f2b857e80d417901465d77eafb13156883b367
Fridmaster <fridmaster@gmaoil>
2017-11-14 17:41:24 +0200

    fix for stories

--- stories/ng2-component-lab/tiles.component.exp.ts
+++ stories/ng2-component-lab/tiles.component.exp.ts
@@ -3,21 +3,21 @@ const alert1  = window.alert;
 const console1  = console.log;
 
 export default experimentOn('Tiles')
-    .case('Tile Sample', {
+    .case('Tile Sample purple', {
         context: {
             alert: alert1,
             console: console1
         },
         showSource: true,
-        template: `<sdc-tile [color] = "'purple'" [header] = "'R'"  [title] = "'Router'" [pre_title] = "'V 0.1'"  [footer] ="'Ready'" (tileClick)="alert($event)"></sdc-tile>`
-    }).case('Tile Sample', {
+        template: `<sdc-tile [color] = "'purple'" [header] = "'R'"  [title] = "'Router'" [pre_title] = "'V 0.1'"  [footer] ="'Ready'" (tileClick)="alert($event)" [footer_icon]="true" ></sdc-tile>`
+    }).case('Tile Sample blue', {
         context: {
             alert: alert1,
             console: console1
         },
         showSource: true,
-        template: `<sdc-tile [color] = "'blue'" [header] = "'S'"  [title] = "'Router2'" [pre_title] = "'V 0.2'"  [footer] ="'Pending'" (tileClick)="alert($event)"></sdc-tile>`
-    }).case('Tile Sample', {
+        template: `<sdc-tile [color] = "'blue'" [header] = "'S'"  [title] = "'Router2'" [pre_title] = "'V 0.2'"  [footer] ="'Pending'" (tileClick)="alert($event)" [footer_icon]="true"></sdc-tile>`
+    }).case('Tile Sample without footer icon', {
         context: {
             alert: alert1,
             console: console1
