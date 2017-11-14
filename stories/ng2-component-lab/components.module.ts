db6dd49a0c9feb6c39847aee96fbf2317ea1c474
Fridmaster <fridmaster@gmaoil>
2017-11-14 17:04:23 +0200

    final fix

--- stories/ng2-component-lab/components.module.ts
+++ stories/ng2-component-lab/components.module.ts
@@ -4,11 +4,11 @@ import {CommonModule} from "@angular/common";
 import {ColorsTable} from "./components/colors-table.component";
 import {FormElementsModule} from "../../src/angular/form-elements/form-elements.module";
 import {ButtonsModule} from "../../src/angular/buttons/buttons.module";
+import {TileModule} from "../../src/angular/tiles/tile.module";
 import {ButtonModalExample} from "./components/button-modal-example.component";
 import {ModalModule} from "../../src/angular/modals/modal.module";
 import {InnerContent} from "./components/inner-content-example.component";
 import {PopupMenuModule} from "../../src/angular/popup-menu/popup-menu.module";
-import {TileModule} from "../../lib/angular/tiles/tile.module";
 
 @NgModule({
     declarations: [
@@ -22,8 +22,9 @@ import {TileModule} from "../../lib/angular/tiles/tile.module";
         CommonModule,
         FormElementsModule,
         ButtonsModule,
-        PopupMenuModule,
-        TileModule
+        TileModule,
+        ButtonsModule,
+        PopupMenuModule
     ],
     entryComponents: [InnerContent],
     exports: [
@@ -33,8 +34,9 @@ import {TileModule} from "../../lib/angular/tiles/tile.module";
         ButtonModalExample,
         FormElementsModule,
         ButtonsModule,
-        PopupMenuModule,
-        TileModule
+        TileModule,
+        ButtonsModule,
+        PopupMenuModule
     ],
     providers: [KeysPipe]
 })
--- stories/ng2-component-lab/tiles.component.exp.ts
+++ stories/ng2-component-lab/tiles.component.exp.ts
@@ -10,19 +10,18 @@ export default experimentOn('Tiles')
         },
         showSource: true,
         template: `<sdc-tile [color] = "'purple'" [header] = "'R'"  [title] = "'Router'" [pre_title] = "'V 0.1'"  [footer] ="'Ready'" (tileClick)="alert($event)"></sdc-tile>`
-    })
-    // .case('Tile Sample', {
-    //     context: {
-    //         alert: alert1,
-    //         console: console1
-    //     },
-    //     showSource: true,
-    //     template: `<sdc-tile [color] = "'blue'" [header] = "'S'"  [title] = "'Router2'" [pre_title] = "'V 0.2'"  [footer] ="'Pending'" (tileClick)="alert($event)"></sdc-tile>`
-    // }).case('Tile Sample', {
-    //     context: {
-    //         alert: alert1,
-    //         console: console1
-    //     },
-    //     showSource: true,
-    //     template: `<sdc-tile [color] = "'blue'" [header] = "'VS'"  [title] = "'Router2'" [pre_title] = "'V 1.2'"  [footer] ="'Finished'" (tileClick)="alert($event)"></sdc-tile>`
-    // });
+    }).case('Tile Sample', {
+        context: {
+            alert: alert1,
+            console: console1
+        },
+        showSource: true,
+        template: `<sdc-tile [color] = "'blue'" [header] = "'S'"  [title] = "'Router2'" [pre_title] = "'V 0.2'"  [footer] ="'Pending'" (tileClick)="alert($event)"></sdc-tile>`
+    }).case('Tile Sample', {
+        context: {
+            alert: alert1,
+            console: console1
+        },
+        showSource: true,
+        template: `<sdc-tile [color] = "'blue'" [header] = "'VS'"  [title] = "'Router2'" [pre_title] = "'V 1.2'"  [footer] ="'Finished'" (tileClick)="alert($event)"></sdc-tile>`
+    });
