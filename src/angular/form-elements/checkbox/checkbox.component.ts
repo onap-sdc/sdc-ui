9ec29643fe5e46a2adbd012af697810e4b1c8e24
Fridmaster <fridmaster@gmaoil>
2017-11-15 14:24:43 +0200

    design checkbox

--- src/angular/form-elements/checkbox/checkbox.component.ts
+++ src/angular/form-elements/checkbox/checkbox.component.ts
@@ -5,16 +5,17 @@ import {Component, Input, Output, EventEmitter, ViewEncapsulation} from '@angula
     templateUrl: './checkbox.component.html',
     encapsulation: ViewEncapsulation.None
 })
+
 export class CheckboxComponent {
-
-
     @Input() label:string;
     @Input() checked:boolean;
     @Input() disabled:boolean;
+    @Input() value: any;
     @Output() checkedChange:EventEmitter<any> = new EventEmitter<any>();
-
-    toggleState(newValue:boolean) {
-        this.checkedChange.emit(newValue);
+    toggleState(new_value:boolean) {
+        this.value.state = new_value;
+        this.checkedChange.emit(this.value);
+        alert(this.value.name + ' ' + this.value.state);
     }
 }
 
