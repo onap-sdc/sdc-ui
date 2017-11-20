f4fcf4e038c2906ae77f926fff423ae4305e9301
Fridmaster <fridmaster@gmaoil>
2017-11-20 10:54:16 +0200

    unit test temp

--- src/angular/form-elements/checkbox/checkbox.component.spec.ts
+++ src/angular/form-elements/checkbox/checkbox.component.spec.ts
@@ -1,30 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { CheckboxComponent } from "./checkbox.component";
+import { DebugElement }    from '@angular/core';
+import { By }              from '@angular/platform-browser';
 
-describe('AppComponent', () => {
+describe("Test", ()=>{
+    let component: CheckboxComponent;
+    let de: DebugElement;
+    let element: HTMLElement;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                AppComponent
+                CheckboxComponent
             ],
         }).compileComponents();
+        const fixture = TestBed.createComponent(CheckboxComponent);
+        component = fixture.componentInstance;
+        de = fixture.debugElement.query(By.css('.sdc-tile'));
+        element  = de.nativeElement;
     }));
 
-    it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
+    it("Create Component should be!", async(()=> {
+        expect(component).toBeTruthy();
     }));
-
-    it(`should have as title 'app'`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('app');
-    }));
-
-    it('should render title in a h1 tag', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-    }));
+})
