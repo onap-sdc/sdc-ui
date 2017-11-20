239e15d5aa791e4cd10ba83bbd59eb409c9a7ed1
Fridmaster <fridmaster@gmaoil>
2017-11-20 19:03:59 +0200

    little fixes

--- stories/ng2-component-lab/button.component.exp.ts
+++ stories/ng2-component-lab/button.component.exp.ts
@@ -2,36 +2,41 @@ import {experimentOn} from '@islavi/ng2-component-lab';
 const alert1 = window.alert;
 
 export default experimentOn('Button')
-    .case('Primary Default', {
+    .case('Primary Buttons : Large, Medium, Small', {
         showSource: true,
         context:{
           alert:alert1
         },
-        template: `<sdc-button (btnClick)="alert('hi')"  [text]="'Button'" [type]="'primary'" [disabled]="false" [size]="'large'"></sdc-button>
-                    <sdc-button (btnClick)="alert('hi')"  [text]="'Button'" [type]="'primary'" [disabled]="false" [size]="'medium'"></sdc-button>
-                     <sdc-button (btnClick)="alert('hi')"  [text]="'Button'" [type]="'primary'" [disabled]="false" [size]="'small'"></sdc-button>`,
-    }).case('Primary Default', {
+        template: `
+                    <sdc-button [text]="'primary'" [type]="'primary'" [disabled]="false" [size]="'large'"></sdc-button>
+                    <sdc-button  [text]="'primary'" [type]="'primary'" [disabled]="false" [size]="'medium'"></sdc-button>
+                     <sdc-button  [text]="'primary'" [type]="'primary'" [disabled]="false" [size]="'small'"></sdc-button>`,
+    }).case('Primary Button Disabled', {
         showSource: true,
         context:{
             alert:alert1
         },
-        template: `<sdc-button (btnClick)="alert('hi')"  [text]="'Button'" [type]="'primary'" [disabled]="true"></sdc-button>`,
-    }).case('White Default', {
+        template: `<sdc-button [text]="'primary'" [type]="'primary'" [disabled]="true"></sdc-button>`,
+    }).case('White Buttons : Large, Medium, Small', {
         showSource: true,
         context:{
             alert:alert1
         },
-        template: `<sdc-button (btnClick)="alert('hi')"  [text]="'Button'" [type]="'white'" [disabled]="false"></sdc-button>`,
-    }).case('White Default, Disabled', {
+        template: `
+            <sdc-button  [text]="'primary'" [type]="'white'" [disabled]="false" [size]="'large'" ></sdc-button>
+            <sdc-button  [text]="'primary'" [type]="'white'" [disabled]="false" [size]="'medium'"></sdc-button>
+            <sdc-button  [text]="'primary'" [type]="'white'" [disabled]="false" size="'small'"></sdc-button>
+        `,
+    }).case('White Button Disabled', {
         showSource: true,
         context:{
             alert:alert1
         },
-        template: `<sdc-button (btnClick)="alert('hi')"  [text]="'Button'" [type]="'white'" [disabled]="true"></sdc-button>`,
-    }).case('White Default, Disabled', {
+        template: `<sdc-button [text]="'primary'" [type]="'white'" [disabled]="true"></sdc-button>`,
+    }).case('Link Button', {
         showSource: true,
         context:{
             alert:alert1
         },
-        template: `<sdc-button (btnClick)="alert('hi')"  [text]="'Button'" [type]="'link'" [disabled]="false"></sdc-button>`,
+        template: `<sdc-button [text]="'primary'" [type]="'link'" [disabled]="false"></sdc-button>`,
     });
