7178f756e04aa06e01aed0517c41ba4ae772fd11
ayelet@msbitsoftware.com <ayelet@msbitsoftware.com>
2017-11-21 13:20:30 +0200

    Added headless drop-down as well as drop-down trigger directive

--- src/angular/form-elements/dropdown/dropdown.component.ts
+++ src/angular/form-elements/dropdown/dropdown.component.ts
@@ -1,28 +1,263 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core'
-
-export class DropdownValue {
-    value:any;
-    label:string;
-
-    constructor(value:any, label:string) {
-        this.value = value;
-        this.label = label;
-    }
-}
+import {Component, EventEmitter, Input, Output, forwardRef, OnInit, OnChanges, SimpleChanges} from '@angular/core'
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {IDropDownGroup, IDropDownItem, IDropDownGroupResult} from "./dropdown-models";
+import {DropDownGroup, DropDownItem} from "../../../../lib/angular/form-elements/dropdown/dropdown.component"
 
 @Component({
     selector: 'sdc-dropdown',
     templateUrl: './dropdown.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => DropDownComponent),
+            multi: true
+        }
+    ],
+    host: {
+        '(document:click)': 'onClickOutside($event)',
+    }
 })
-export class DropDownComponent {
+export class DropDownComponent implements ControlValueAccessor, OnChanges {
 
+
+    /**
+     * Drop-down value changed event emitter
+     */
     @Output('valueChange') baseEmitter:EventEmitter<any> = new EventEmitter<any>();
 
-    @Input() label:string;
-    @Input() options:DropdownValue[] | string[];
-    @Input() value:any;
-    @Input() disabled:boolean;
+    /**
+     * The label that will show up above the drop-down
+     */
+    @Input() label: string;
+
+    /**
+     * Option can be add by a list of IDropDownGroup, IDropDownItem objects, or a list of strings
+     */
+    @Input() options: (IDropDownGroup|IDropDownItem|string)[];
+
+    /**
+     * Initial value the drop-down should have
+     */
+    @Input() value: any;
+
+    /**
+     * Drop-down disabled flag
+     */
+    @Input() disabled: boolean;
+
+    /**
+     * The text users will see on the drop-down header when no option was selected
+     */
     @Input() placeHolder:string;
+
+    /**
+     * Drop-down required flag
+     */
     @Input() required:boolean;
+
+
+    @Input() validate:boolean;
+
+    @Input() headless = false;
+
+    /**
+     * Selected option index
+     */
+    public selectedIndex: number;
+
+    /**
+     * Selected option label
+     */
+    public selectedLabel:string;
+
+    /**
+     * The selected drop-down group
+     */
+    public selectedDropDownGroup: IDropDownGroup;
+
+    /**
+     * Drop-down show/hide flag. default is false (closed)
+     * @type {boolean}
+     */
+    public show = false;
+
+    /**
+     * Error flag
+     * @type {boolean}
+     */
+    public error = false;
+
+    /**
+     * Placeholder for 'registerOnChange' function. For ControlValueAccessor registerOnChange method
+     */
+    private propagateChange = (_:any)=>{};
+
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if(changes.value){
+            this.setValueIfExist(this.value);
+        }
+    }
+
+    public isValid(){
+        return this.error;
+    }
+
+    /**
+     * Validate when required is enabled
+     */
+    public validateDropDown(): void{
+        if(!this.disabled && this.required && (!this.value || this.value === '')){
+            this.error = true;
+            return;
+        }
+        this.error = false;
+    }
+
+    /**
+     * Set option as selected and saves it's index on the list
+     * @param index - number
+     * @param option - IDropDownItem or string
+     */
+    public selectOption(index: number, option:any):void{
+        this.selectedIndex = index;
+        this.updateSelected(option.label || option, option.value || option);
+
+        /**
+         * Notify the control a change was made, so that ngModel will later update its value.
+         */
+        this.propagateChange(this.value);
+    }
+
+    /**
+     * Set drop-down group item as selected. Save the selected group as well as the value
+     * @param event
+     */
+    public selectGroupOption(event: IDropDownGroupResult){
+        const item = event.group.items.find((i) => event.value === i.value);
+        this.selectedDropDownGroup = event.group;
+        this.updateSelected(item.label, item.value);
+
+        /**
+         * Notify the control a change was made, so that ngModel will later update its value.
+         */
+        this.propagateChange(this.value);
+    }
+
+    /**
+     * Update the value and label of the drop down with new ones
+     * @param label
+     * @param value
+     */
+    private updateSelected(label: string, value: string, index?: number){
+        this.selectedLabel = label;
+        this.value = value;
+        this.baseEmitter.next(this.value);
+        this.selectedIndex = index || this.selectedIndex;
+        this.validateDropDown();
+    }
+
+    /**
+     * Get the value of selected drop-down group item
+     * @param dropdownGroup
+     * @returns {string}
+     */
+    public getDropDownGroupSelectedValue(dropdownGroup: IDropDownGroup):string{
+        if(this.selectedDropDownGroup && this.selectedDropDownGroup.title === dropdownGroup.title){
+            return this.value;
+        }
+        return null;
+    }
+
+    /**
+     * Check if our options are IDropDownGroup items
+     */
+    public isDropDownGroupList(): boolean{
+        return this.options && this.options.length && this.options[0].hasOwnProperty('title') || false;
+    }
+
+    /**
+     * Check if our options are IDropDownItem items
+     */
+    private isDropDownItemList(): boolean{
+        return this.options && this.options.length && this.options[0].hasOwnProperty('label') || false
+    }
+
+    /**
+     * Get the label of the selected option
+     */
+    public getSelectedLabel(): string{
+       return this.selectedLabel || null;
+    }
+
+    /**
+     * Toggle show/hide drop-down list
+     */
+    public toggleDropdown(){
+        if(!this.disabled){
+            this.show = !this.show;
+        }
+    }
+
+    /**
+     * When users clicks outside the drop-down it will be closed
+     */
+    public onClickOutside(event){
+        if(!event.target.classList.contains('js-sdc-dropdown--toggle-hook')){
+            this.show = false;
+        }
+    }
+
+    private setValueIfExistOnGroupList(value: string): void{
+        const selectedDropDownGroup = this.options.find((dropDownGroup,index)=>{
+            const checkDropDownGroup = <IDropDownGroup>dropDownGroup;
+            const callback = this.setFoundValueCallback.bind(this, true, value);
+            return !!checkDropDownGroup.items.find(callback);
+        });
+        if(selectedDropDownGroup){
+            this.selectedDropDownGroup = <IDropDownGroup>selectedDropDownGroup;
+        }
+    }
+
+   private setFoundValueCallback(isItem: boolean, value: string, option: any, index:number){
+        const optionValue = isItem?(<IDropDownItem>option).value:option;
+        if(optionValue === value){
+            const optionLabel = isItem?(<IDropDownItem>option).label:option;
+            this.updateSelected(optionLabel, optionValue, index);
+            return true;
+        }
+        return false;
+   }
+
+    /**
+     * Test if the specified value exist on the option list, if it is,
+     * add it as the enw value
+     * @param value
+     */
+    private setValueIfExist(value:string): void{
+        if(this.isDropDownGroupList()){
+            this.setValueIfExistOnGroupList(value);
+        }else if(this.isDropDownItemList()){
+            this.options.find(this.setFoundValueCallback.bind(this, true, value));
+        }else{
+            this.options.find(this.setFoundValueCallback.bind(this, false, value));
+        }
+    }
+
+    /**
+     * ControlValueAccessor interface methods.
+     */
+    writeValue(val: any): void {
+        if(val && typeof val === 'string'){
+            this.setValueIfExist(val);
+        }
+    }
+
+    registerOnChange(fn: any): void {
+        this.propagateChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {};
+
 
 }
