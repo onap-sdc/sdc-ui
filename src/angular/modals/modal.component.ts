852f59fc77f8b61934827fdabce5d363be16af9f
Nechama Gordon <ng689e@intl.att.com>
2017-12-18 08:47:18 +0200

    Adding animation

--- src/angular/modals/modal.component.ts
+++ src/angular/modals/modal.component.ts
@@ -2,14 +2,25 @@
  * Created by rc2122 on 6/1/2017.
  */
 import {
-    Component, Input, ViewContainerRef, ViewChild, ComponentRef
+    Component, Input, ViewContainerRef, ViewChild, ComponentRef,
+    trigger, state, animate, transition, style
 } from '@angular/core';
 import template from './modal.component.html';
 import { ModalButtonComponent } from './modal-button.component';
 
 @Component({
     selector: 'sdc-modal',
-    template: template
+    template: template, 
+    animations: [
+        trigger('toggleBackground', [
+            transition(':enter', [style({opacity:0}), animate('.45s cubic-bezier(0.23, 1, 0.32, 1)')]),
+            transition(':leave', [animate('1s cubic-bezier(0.23, 1, 0.32, 1)', style({opacity:1}))])
+        ]),
+        trigger('toggleModal', [
+           transition(':enter', [style({opacity:0, transform: 'translateY(-60px)'}),  animate('.45s cubic-bezier(0.23, 1, 0.32, 1)')]),
+           transition(':leave', [animate('1s ease-in-out', style({opacity:1, transform: 'translateY(-60px)'}))])
+        ])
+    ]
 })
 
 export class ModalComponent{
