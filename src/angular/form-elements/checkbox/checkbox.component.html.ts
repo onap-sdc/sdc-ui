3842bd953aa3e3467234acf90d5d20ed279b6b6e
Nechama Gordon <ng689e@intl.att.com>
2017-12-21 22:12:49 +0200

    updating class name

--- src/angular/form-elements/checkbox/checkbox.component.html.ts
+++ src/angular/form-elements/checkbox/checkbox.component.html.ts
@@ -1,6 +1,6 @@
 export default `
 <div class="sdc-checkbox">
-    <div class="animation-container" ripple-click-animation>
+    <div class="sdc-checkbox__animation-wrapper" ripple-click-animation>
         <input type="checkbox" class="sdc-checkbox__input" [ngModel]="checked" (ngModelChange)="toggleState($event)" [disabled]="disabled">
     </div>
     <label class="sdc-checkbox__label" *ngIf="label" >{{ label }}</label>
