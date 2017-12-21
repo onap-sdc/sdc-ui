6079bae855e63dbbd73d73295dcb4a60324988b7
Nechama Gordon <ng689e@intl.att.com>
2017-12-21 20:10:43 +0200

    Adding animation to radio buttons

--- src/angular/form-elements/radios/radio-buttons-group.component.html.ts
+++ src/angular/form-elements/radios/radio-buttons-group.component.html.ts
@@ -2,8 +2,8 @@ export default `
 <label class='sdc-radio-group__legend'>{{legend}}</label>
 <div class='sdc-radio-group__radios {{direction}}'>
     <template ngFor let-item="$implicit" [ngForOf]="options.items">
-        <div class="sdc-radio">
-            <input class = "sdc-radio__input"
+        <div class="sdc-radio" ripple-click-animation>
+            <input class = "sdc-radio__input" 
                 type="radio"
                 name="{{item.name}}"
                 value="{{item.value}}"
