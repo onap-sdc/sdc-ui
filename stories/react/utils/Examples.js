d301725fd07dbf61f775a634393eb30a59f6d34d
stanvishne <shlomo-stanisla.vishnevetskiy@amdocs.com>
2018-01-02 18:18:18 +0200

    html preview for react modals component

--- stories/react/utils/Examples.js
+++ stories/react/utils/Examples.js
@@ -8,14 +8,14 @@ const Examples = ({examples}) => (
   <div className={'examples'}>
 	  {Object.keys(examples).map(key => {
 		let title = key;
-		let {jsx, html, displayTitle = true, exclude} = examples[key];
+		let {jsx, html, displayTitle = true, exclude, renderFromJsx = false} = examples[key];
 		if (!html) {
 			html = renderToStaticMarkup(jsx);
 			html = beautifyHTML({html, indentChar: '  '});
 		} else {
 			html = insertSVGIcons({html, jsx});
 		}
-		return <SourceToggle title={displayTitle && title} jsx={jsx} html={html} key={key} exclude={exclude}/>;
+		return <SourceToggle title={displayTitle && title} jsx={jsx} html={html} key={key} exclude={exclude} renderFromJsx={renderFromJsx}/>;
 	  })}
   </div>
 );
