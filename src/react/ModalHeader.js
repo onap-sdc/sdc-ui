3a8baebfd1e500674c20843611be3f1feace97fc
fridman <fridmaster@gmail.com>
2018-03-05 10:50:05 +0200

    fix react tests

--- src/react/ModalHeader.js
+++ src/react/ModalHeader.js
@@ -8,14 +8,17 @@ const iconMaper = {
 	alert: 'exclamationTriangleLine'
 };
 
-const headerType = {
+const headerTypes = {
     error: 'sdc-error__header',
     info: 'sdc-info__header',
+    alert: 'sdc-alert__header',
     custom: 'sdc-custom__header'
 }
 
+
+
 const Header = ({children, onClose, type}) => (
-    <div className={headerType[type] + ' sdc-modal__header'} >
+    <div className={ headerTypes[type] + ' sdc-modal__header'} >
         {type !== 'custom'
             &&  
                     <SVGIcon iconClassName='sdc-modal__icon' className='sdc-modal__svg-use'  name={iconMaper[type]}/>   
