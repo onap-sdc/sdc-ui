2855656b5ee7967ba67e7a7584492984eed5b55e
islavi <israel.lavi@gmail.com>
2018-04-01 16:20:34 +0300

    Adding tabs support for icons.

--- src/angular/tabs/tabs.component.html.ts
+++ src/angular/tabs/tabs.component.html.ts
@@ -1,7 +1,13 @@
 export default `
 <ul class="sdc-tabs-list" role="tablist">
     <li *ngFor="let tab of tabs" class="sdc-tab" role="tab" (click)="selectTab(tab)" [class.sdc-tab-active]="tab.active">
-        {{tab.title}}
+        <span *ngIf="tab.title">{{tab.title}}</span>
+        <svg-icon-label
+            *ngIf="tab.titleIcon"
+            [name]="tab.titleIcon"
+            [mode]="tab.titleIconMode"
+            [size]="_size">
+        </svg-icon-label>
     </li>
 </ul>
 <ng-content></ng-content>
