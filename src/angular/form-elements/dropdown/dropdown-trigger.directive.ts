373e2da4946f50ce776dd11537d03bb37fbf76d8
islavi <israel.lavi@gmail.com>
2018-04-24 16:20:56 +0300

    Fix dropdown functionality and styles.

--- src/angular/form-elements/dropdown/dropdown-trigger.directive.ts
+++ src/angular/form-elements/dropdown/dropdown-trigger.directive.ts
@@ -1,23 +1,17 @@
-import { Directive, Input, OnInit, HostBinding, HostListener } from "@angular/core";
+import { Directive, Input, HostBinding, HostListener } from "@angular/core";
 import { DropDownComponent } from "./dropdown.component";
 
 @Directive({
-    selector: '[dropdown-trigger]'
+    selector: '[SdcDropdownTrigger]'
 })
 
-export class DropDownTriggerDirective implements OnInit{
+export class DropDownTriggerDirective {
 
     @HostBinding('class.js-sdc-dropdown--toggle-hook') true;
     @Input() dropDown: DropDownComponent;
 
-    @HostListener('click') onClick = () => {
-        this.dropDown.toggleDropdown();
-    }
-
-    ngOnInit = (): void => {
-        if (this.dropDown) {
-            this.dropDown.headless = true;
-        }
+    @HostListener('click', ['$event']) onClick = (event) => {
+        this.dropDown.toggleDropdown(event);
     }
 
 }
