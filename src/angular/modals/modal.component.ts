8547ec4751f32021816a095fb351df1aeafe20a1
islavi <israel.lavi@gmail.com>
2018-04-26 15:34:43 +0300

    1. Adding option to change modal title and buttons.
    2. Expose create inner component function.

--- src/angular/modals/modal.component.ts
+++ src/angular/modals/modal.component.ts
@@ -8,39 +8,60 @@ import {RippleAnimationAction} from "../animations/ripple-click.animation.direct
     template: template,
     animations: [
         trigger('toggleBackground', [
-            transition('* => 1', [style({opacity:0}), animate('.45s cubic-bezier(0.23, 1, 0.32, 1)')]),
-            transition('1 => *', [animate('.35s cubic-bezier(0.23, 1, 0.32, 1)', style({opacity:0}))])
+            transition('* => 1', [style({opacity: 0}), animate('.45s cubic-bezier(0.23, 1, 0.32, 1)')]),
+            transition('1 => *', [animate('.35s cubic-bezier(0.23, 1, 0.32, 1)', style({opacity: 0}))])
         ]),
         trigger('toggleModal', [
-            transition('* => 1', [style({opacity:0, transform: 'translateY(-80px)'}),  animate('.45s cubic-bezier(0.23, 1, 0.32, 1)')]),
-            transition('1 => *', [style({opacity:1, transform: 'translateY(0px)'}), animate('.35s ease-in-out', style({opacity:0, transform: 'translateY(-80px)'}))])
+            transition('* => 1', [style({opacity: 0, transform: 'translateY(-80px)'}),  animate('.45s cubic-bezier(0.23, 1, 0.32, 1)')]),
+            transition('1 => *', [style({opacity: 1, transform: 'translateY(0px)'}), animate('.35s ease-in-out', style({opacity:0, transform: 'translateY(-80px)'}))])
         ])
     ]
 })
 
 export class ModalComponent{
 
-    @Input() size:string; 'xl|l|md|sm|xsm';
-    @Input() title:string;
-    @Input() message:string;
-    @Input() buttons:Array<ModalButtonComponent>;
-    @Input() type:string; 'info|error|alert|custom';
-    @Output() closeAnimationComplete:EventEmitter<any> = new EventEmitter<any>();
-    modalVisible: boolean = true;
-    //Allows for custom component as body instead of simple message. See ModalService.createActionModal for implementation details, and HttpService's catchError() for example.
-    @ViewChild('dynamicContentContainer', {read: ViewContainerRef}) dynamicContentContainer:ViewContainerRef;
-    innerModalContent:ComponentRef<ModalComponent>;
+    @Input() size: string; 'xl|l|md|sm|xsm';
+    @Input() title: string;
+    @Input() message: string;
+    @Input() buttons: ModalButtonComponent[];
+    @Input() type: string; 'info|error|alert|custom';
+    @Output() closeAnimationComplete: EventEmitter<any> = new EventEmitter<any>();
+    modalVisible: boolean;
+    // Allows for custom component as body instead of simple message.
+    // See ModalService.createActionModal for implementation details, and HttpService's catchError() for example.
+    @ViewChild('dynamicContentContainer', {read: ViewContainerRef}) dynamicContentContainer: ViewContainerRef;
+    innerModalContent: ComponentRef<ModalComponent>;
 
-    public rippleAnimationAction:RippleAnimationAction = RippleAnimationAction.MOUSE_ENTER;
+    public rippleAnimationAction: RippleAnimationAction = RippleAnimationAction.MOUSE_ENTER;
 
-    constructor(private renderer:Renderer){}
+    constructor(private renderer: Renderer) {
+        this.modalVisible = true;
+    }
 
-    public modalToggled = (toggleEvent:any) => {
-        if(!toggleEvent.toState) this.closeAnimationComplete.emit();
-    };
+    public modalToggled = (toggleEvent: any) => {
+        if (!toggleEvent.toState) {
+            this.closeAnimationComplete.emit();
+        }
+    }
 
-    public hoverAnimation(evn: MouseEvent){
-        this.renderer.setElementClass(<HTMLElement> evn.target, 'sdc-ripple-click__animated', true);
-        //evn.taregt.classList.add('sdc-ripple-click__animated');
+    public getButtons = (): ModalButtonComponent[] => {
+        return this.buttons;
+    }
+
+    public setButtons = (_buttons: ModalButtonComponent[]): void => {
+        this.buttons = _buttons;
+    }
+
+    public getTitle = (): string => {
+        return this.title;
+    }
+
+    public setTitle = (_title: string): void => {
+        this.title = _title;
+    }
+
+    public hoverAnimation(evn: MouseEvent) {
+        this.renderer.setElementClass(evn.target as HTMLElement, 'sdc-ripple-click__animated', true);
+        // evn.taregt.classList.add('sdc-ripple-click__animated');
     }
 }
