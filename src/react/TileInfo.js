ac5721c5b21769f825a69ebfdee4e253f58e28cf
Stanislav Vishnevetskiy <svishne@amdocs.com>
2018-07-03 13:54:55 +0300

    Tile hot-reload fix

--- src/react/TileInfo.js
+++ src/react/TileInfo.js
@@ -1,13 +1,15 @@
 import React, { Children } from 'react';
 import TileInfoLine from './TileInfoLine.js';
-const TileInfoLineType = (() => <TileInfoLine />)().type;
 
 const TileInfo = ({ align, children }) => (
     <div
         className={`sdc-tile-content-info ${
             align === 'center' ? 'centered' : ''
         }`}>
-        {Children.toArray(children).filter(e => e.type === TileInfoLineType)}
+        {Children.toArray(children).filter(
+            e =>
+                e.type.displayName === 'TileInfoLine' || e.type === TileInfoLine
+        )}
     </div>
 );
 
