e7e3ea85549700eb24db198f05ad8c16d32c1699
Stanislav Vishnevetskiy <svishne@amdocs.com>
2018-07-03 10:17:45 +0300

    variable name fix

--- src/react/TileInfo.js
+++ src/react/TileInfo.js
@@ -1,13 +1,13 @@
 import React, { Children } from 'react';
 import TileInfoLine from './TileInfoLine.js';
-const LineType = (() => <TileInfoLine />)().type;
+const TileInfoLineType = (() => <TileInfoLine />)().type;
 
 const TileInfo = ({ align, children }) => (
     <div
         className={`sdc-tile-content-info ${
             align === 'center' ? 'centered' : ''
         }`}>
-        {Children.toArray(children).filter(e => e.type === LineType)}
+        {Children.toArray(children).filter(e => e.type === TileInfoLineType)}
     </div>
 );
 
